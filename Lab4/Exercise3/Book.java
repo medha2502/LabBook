4136f78a1145babdb979a1e52798c8700a6ba228
medha2502 <68118356+medha2502@users.noreply.github.com>
2021-05-23 23:52:30 +0530

    Add files via upload

---
+++ Lab4/Exercise3/Book.java
@@ -0,0 +1,17 @@
+package com.capgemini.exercise3;
+
+
+	
+public class Book extends WrittenItem{
+
+	public Book(int number, String title, int numberOfCopies, String authorName) {
+		super(number, title, numberOfCopies, authorName);
+	}
+
+	public Book() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+    
+}
